378294cda23c2fe6ece94796cb25b3fd596ec6d6
Rafael Ferreira <osmarditto@gmail.com>
2022-06-05 22:45:22 -0300

    ProductAPI Models

---
+++ VShop.ProductAPI/Context/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProductAPI.Models;
+
+namespace VShop.ProductAPI.Context;
+
+public class AppDBContext : DbContext
+{
+  public DbSet<Category> Categories { get; set; }
+  public DbSet<Product> Products { get; set; }
+
+  public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+}
